e0f0749e3c903ff01b12651f5baf8717f394d114
Isaeva Adelina <isaeva.adelya2016@ya.ru>
2025-02-02 16:17:12 +0300

    fix(facebook): work

--- ads-integration-service/csv_prepare.py
+++ ads-integration-service/csv_prepare.py
@@ -1,8 +1,75 @@
 import hashlib
 import csv
 
+
 def hash_data(data):
-    return hashlib.sha256(data.encode('utf-8')).hexdigest()
+    return hashlib.sha256(str(data).encode('utf-8')).hexdigest()
+
+def format_users(applications):
+    users = []
+    for user in applications:
+        formatted_user = []
+
+        phone = user.get("contacts_buy_phones", None)
+        if phone:
+            formatted_user.append(hash_data(phone).replace("+.", ""))
+        else:
+            continue
+        email = user.get("contacts_buy_emails", None)
+        if email:
+            formatted_user.append(hash_data(email))
+        else:
+            formatted_user.append("")
+        external_id = user.get("contacts_id", None)
+        if external_id:
+            formatted_user.append(hash_data(str(external_id)))
+        else:
+            formatted_user.append("")
+        gender = user.get("contacts_buy_sex", None)
+        if gender:
+            formatted_user.append(hash_data(gender))
+        else:
+            formatted_user.append("")
+        country = user.get("contacts_buy_geo_country_name", "Узбекистан")
+        if country:
+            formatted_user.append(hash_data(country))
+        else:
+            formatted_user.append("")
+        city = user.get("contacts_buy_geo_city_name", "Ташкент")
+        if city:
+            formatted_user.append(hash_data(city))
+        else:
+            formatted_user.append("")
+        # name_full = user.get("contacts_buy_name", None)
+        # if name_full:
+        #     words = name_full.split()
+        #     formatted_user.append(hash_data(words[0]))
+        #     if len(words) > 1:
+        #         formatted_user.append(hash_data(words[1]))
+        name_first = user.get("name_first", None)
+        if name_first:
+            formatted_user.append(hash_data(name_first))
+        else:
+            formatted_user.append("")
+        name_last = user.get("name_last", None)
+        if name_last:
+            formatted_user.append(hash_data(name_last))
+        else:
+            formatted_user.append("")
+        dob = user.get("contacts_buy_dob", None)
+        if dob:
+            dob_str = dob.__str__()
+            ymd = dob_str.split("-")
+            if len(ymd) == 3:
+                formatted_user.append(hash_data(ymd[0]))
+                formatted_user.append(hash_data(ymd[1]))
+                formatted_user.append(hash_data(ymd[2]))
+        else:
+            formatted_user.append("")
+            formatted_user.append("")
+            formatted_user.append("")
+        users.append(formatted_user)
+    return users
 
 def prepare_user(application):
     user = {}
