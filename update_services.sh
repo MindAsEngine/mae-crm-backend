49581f386c5d93ca13243b91c03f45139aa06643
ProdamGaraj <prodgaraj@yandex.ru>
2025-02-24 18:42:48 +0500

    production update

--- update_services.sh
+++ update_services.sh
@@ -9,15 +9,15 @@ docker rmi mae-crm-backend-frontend
 docker rmi mae-crm-backend-ads-integration-service
 docker rmi mae-crm-backend-auth-service
 
-# Fetch and pull the latest changes from the main repository
-git fetch --all
-git pull
+# # Fetch and pull the latest changes from the main repository
+# git fetch --all
+# git pull
 
-# Fetch and pull the latest changes from the second repository
-cd mae-crm-frontend
-git fetch --all
-git pull
-cd ..
+# # Fetch and pull the latest changes from the second repository
+# cd mae-crm-frontend
+# git fetch --all
+# git pull
+# cd ..
 
 # Start docker-compose services
 docker-compose up -d
